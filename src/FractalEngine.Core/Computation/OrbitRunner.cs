114bbaa2a9bdecd3046c35d47ac6c7b3c4fe8786
Will Greenwood <williamgreenwood2004@gmail.com>
2026-05-12 01:38:09 -0400

    Temp OrbitRunner class

---
+++ src/FractalEngine.Core/Computation/OrbitRunner.cs
@@ -0,0 +1,22 @@
+﻿using FractalEngine.Core.Fractals;
+using System.Numerics;
+
+namespace FractalEngine.Core.Computation
+{
+    public class OrbitRunner
+    {
+        private readonly IFractal _fractal;
+        private readonly Complex _point;
+
+        public OrbitRunner(IFractal fractal, Complex point)
+        {
+            _fractal = fractal;
+            _point = point;
+        }
+
+        public int GetIterationCount()
+        {
+            return _fractal.GetIterationCount(_point);
+        }
+    }
+}
