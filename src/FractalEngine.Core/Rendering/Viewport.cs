bbffe84941617d4ac551ba2685e2c3bf29e95535
Will Greenwood <williamgreenwood2004@gmail.com>
2026-05-15 15:55:00 -0400

    Basic Viewport class created

---
+++ src/FractalEngine.Core/Rendering/Viewport.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace FractalEngine.Core.Rendering
+{
+    public class Viewport()
+    {
+        private int _width { get; }
+        private int _height { get; }
+        private Complex _center { get; }
+        private double _planeWidth { get; }
+
+        private double _minRe { get; }
+        private double _maxRe { get; }
+        private double _minIm { get; }
+        private double _maxIm { get; }
+
+        public Viewport(
+            int width,
+            int height,
+            Complex center,
+            double planeWidth
+            ) : this()
+        {
+            _width = width;
+            _height = height;
+            _center = center;
+            _planeWidth = planeWidth;
+
+            _minRe = _center.Real - (_planeWidth / 2);
+            _maxRe = _center.Real + (_planeWidth / 2);
+            _minIm = _center.Imaginary - (_planeWidth / 2);
+            _maxIm = _center.Imaginary + (_planeWidth / 2);
+        }
+
+        public Complex PixelToPoint(double real, double imaginary)
+        {
+            double re = _minRe + (real / _width) * (_maxRe - _minRe);
+            double im = _minIm + (imaginary / _height) * (_maxIm - _minIm);
+            return new Complex(re, im);
+        }
+    }
+}
